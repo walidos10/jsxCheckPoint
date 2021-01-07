2e7cb601f109e83fb6f6028cded806602f691307
walidos10 <walid-attia@live.fr>
2021-01-07 14:46:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import imgSource from "./imageInSrc.jpg";
+import "./App.css";
+import "./Style.css";
 
 function App() {
+  let name = "Attia Ghazi Walid";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">{name}</h1>
+
+        <br />
+        <img src={imgSource} alt="imageInSrc" />
+        <br />
+        <img src="/imageInPublic.jpg" alt="imageInPublic" />
+      </div>
+      <video width={320} height={240} controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
